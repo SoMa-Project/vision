71c686154ebf758240bb62add49c2e9afa3e7e1f
Can Erdogan <can.erdogan@tu-berlin.de>
2017-05-26 10:53:35 +0200

    Created a .wrl file. Next: Create .xml file that shows connectivity between planes

--- ecto_cerrt/src/createWRL.cpp
+++ ecto_cerrt/src/createWRL.cpp
@@ -27,7 +27,7 @@ void chatterCallback(const visualization_msgs::MarkerArray::ConstPtr& msg) {
 	Quaternion <double> camQ (-0.340, 0.790, -0.469, 0.202);
 	Matrix3d camR = camQ.toRotationMatrix();
   Vector3d camT (0.000, -0.700, 1.400);
-	cout << camR << endl;
+	// cout << camR << endl;
 
 	// Get the polygon data
 	vector < vector <Vector3d> > polygons;
@@ -40,12 +40,12 @@ void chatterCallback(const visualization_msgs::MarkerArray::ConstPtr& msg) {
 
 			// Save the list of points 
 			vector <Vector3d> poly;
-			cout  << "0 0 0" << endl;
+			// cout  << "0 0 0" << endl;
 			Vector3d mean (0,0,0);
 			for(int j = 0; j < m.points.size(); j++) {
 				Vector3d loc = Vector3d(m.points[j].x, m.points[j].y, m.points[j].z);
 				Vector3d worldP = camR * loc + camT;
-				cout <<  worldP.transpose() << endl;
+				// cout <<  worldP.transpose() << endl;
 				poly.push_back(worldP);
 				if(j < (m.points.size() - 1)) mean += worldP;
 			}
@@ -56,7 +56,7 @@ void chatterCallback(const visualization_msgs::MarkerArray::ConstPtr& msg) {
 	}
 
 	// Compute the plane parameters for each polygon
-	cout  << "0 0 0" << endl;
+	// cout  << "0 0 0" << endl;
 	vector <Vector4d> planes;
 	for(int poly = 0; poly < polygons.size(); poly++) {
 		Vector3d p1 = polygons[poly][0];
@@ -67,28 +67,77 @@ void chatterCallback(const visualization_msgs::MarkerArray::ConstPtr& msg) {
 		Vector3d n = (d21.cross(d31)).normalized();
 		double d = -n.dot(p1);
 		planes.push_back(Vector4d(n(0), n(1), n(2), d));
-		cout << n.transpose() << endl;
+		// cout << n.transpose() << endl;
 	}
+
+	// Print the header
+	printf("#VRML V2.0 utf8\n"
+		"#IndexedFaceSet polygons\n"
+		"Transform {\n"
+  	"\ttranslation 0.84999996 0 -0.69999998\n"
+		"\tchildren [\n");
 
 	// For each polygon, create a shape in wrl file
 	set <int> connections;
-	for(int poly1 = 0; poly1 < polygons.size(); poly1++) {
-		printf("\t\tTransform {\n
-      translation 
-      children 
-				Shape {
-					appearance Appearance{
-						material Material { 
-							diffuseColor     1 0 0 #simple red
-							}
-						}
-					geometry IndexedFaceSet {
-						coord Coordinate {
-														point [
-	
+	for(int poly = 0; poly < polygons.size(); poly++) {
+	//for(int poly = 0; poly < 1; poly++) {
+
+		// print the header
+		printf("\t\tTransform {\n"
+			"\t\t\ttranslation %lf %lf %lf\n"
+      "\t\t\tchildren \n"
+			"\t\t\tShape {\n"
+			"\t\t\t\tappearance Appearance{\n"
+			"\t\t\t\t\tmaterial Material { \n"
+			"\t\t\t\t\t\tdiffuseColor %lf %lf %lf\n"
+			"\t\t\t\t\t}\n"
+			"\t\t\t\t}\n"
+			"\t\t\t\tgeometry IndexedFaceSet {\n"
+			"\t\t\t\t\tcoord Coordinate {\n"
+			"\t\t\t\t\t\tpoint [\n", 
+				means[poly](0), means[poly](1), means[poly](2),
+				1.0, 0.0, 0.0); 
+
+		// Print the top side of the polyhedra
+		printf("\t\t\t\t\t\t\t# top\n");
+		for(int j = 0; j < (polygons[poly].size() - 1); j++) {
+			Vector3d p = polygons[poly][j] - means[poly];
+			printf("\t\t\t\t\t\t\t%lf %lf %lf,\n", p(0), p(1), p(2));
+		}
+
+		// Print the bottom side of the polyhedra
+		printf("\t\t\t\t\t\t\t# bottom\n");
+		for(int j = 0; j < (polygons[poly].size() - 1); j++) {
+			Vector3d p = polygons[poly][j] - means[poly];
+			Vector3d n = planes[poly].block<3,1>(0,0);
+			Vector3d p2 = p + n * 0.01;
+			printf("\t\t\t\t\t\t\t%lf %lf %lf,\n", p2(0), p2(1), p2(2));
+		}
+		printf("\t\t\t\t\t\t]\n\t\t\t\t\t}\n");
+
+		// Create the face indices
+		int nV = (polygons[poly].size() - 1);
+		printf("\t\t\t\t\tcoordIndex [\n\t\t\t\t\t\t");		
+		// ... top face (note the backwards)
+		for(int j = 0; j < nV; j++) printf("%d, ", (nV-j)%nV);
+		printf("0, -1, \n\t\t\t\t\t\t");
+		// ... bottom face
+		for(int j = 0; j < nV; j++) printf("%d, ", j + nV);
+		printf("%d, -1, \n", nV);
+		// ... side faces
+		for(int j = 0; j < nV; j++) 
+			printf("\t\t\t\t\t\t%d, %d, %d, %d, %d, -1\n", j, ((j+1)%nV), ((j+1)%nV)+nV, (j+nV), j);
+		printf("\t\t\t\t\t]\n");
+		
+		// Wrap up
+		printf("\t\t\t\t}\n");
+		printf("\t\t\t}\n");
+		printf("\t\t}\n");
 	}
 
-  ROS_INFO("I heard! %d\n", polygons.size());
+	printf("\t]\n}\n");
+
+  // ROS_INFO("I heard! %d\n", polygons.size());
 	exit(1);
 }
 
